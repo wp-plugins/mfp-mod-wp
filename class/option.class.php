17ee19e099836c138f2093412e40e30401424afd
Varrcan <Varrcan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-25 22:59:11 +0000

    add fix
    git-svn-id: https://plugins.svn.wordpress.org/mfp-mod-wp/trunk@1099727 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- class/option.class.php
+++ class/option.class.php
@@ -311,7 +311,7 @@ class mainMfp{
 			add_action('login_enqueue_scripts', array(&$this, 'custom_login_css'), 100); // CSS
 			//add_action('login_message', array(&$this, 'custom_login_form')); // Сообщение над формой
 			//add_action('login_footer', array(&$this, 'custom_login_footer')); // Футтер
-			add_filter('login_headerurl', function(){ return get_home_url();}); // Смена ссылки с wordpress.org на главную сайта
+			//add_filter('login_headerurl', function(){ return get_home_url();}); // Смена ссылки с wordpress.org на главную сайта
 			add_filter('login_headertitle', function(){ return false;}); // Удаление title логотипа
 		}
   }
