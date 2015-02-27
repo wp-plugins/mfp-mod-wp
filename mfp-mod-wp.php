9117b412d0bb5d78970c607987d1b0315ba3440e
Varrcan <Varrcan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-27 19:50:38 +0000

    Version: 0.3.3
    git-svn-id: https://plugins.svn.wordpress.org/mfp-mod-wp/trunk@1101434 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mfp-mod-wp.php
+++ mfp-mod-wp.php
@@ -2,7 +2,7 @@
 
 Plugin Name: MFP mod WP
 Description: Plugin MFP mod WP does two main functions:  clean your source code from links, which can to slow down your blog and hides some articles such as version of the engine, links to wordpress.org etc. from the admintool.
-Version: 0.3.2
+Version: 0.3.3
 Author: Sergey Voloshin
 Author URI: https://varrcan.me/
 Plugin URI: https://varrcan.me/
